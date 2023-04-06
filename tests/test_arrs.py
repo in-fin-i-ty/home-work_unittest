a11c872853c737b14f45ea9cea794a2c18cf1460
in-fin-i-ty <128904385+in-fin-i-ty@users.noreply.github.com>
2023-04-06 23:10:13 +0300

    correct test

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -1,6 +1,6 @@
 import unittest
-from unittest_proj.utils import arrs
-
+#from unittest_proj.utils import arrs
+from utils import arrs
 
 class TestArrs(unittest.TestCase):
 
@@ -11,7 +11,10 @@ class TestArrs(unittest.TestCase):
     def test_slice(self):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+        self.assertEqual(arrs.my_slice([1, 2, 3], -1, 3), [3])
+        self.assertEqual(arrs.my_slice([], 1, 3), [])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, -1), [2, 3])
+        self.assertEqual(arrs.my_slice([1], -7, 3), [1])
 
 
-if __name__ == '__main__':
-    unittest.main()
+
